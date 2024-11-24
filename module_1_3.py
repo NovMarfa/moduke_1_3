cd5189fcc7a8a2e739376128b6d9d1dc915159e5
NovMarfa <mn211195@gmail.com>
2024-11-24 18:07:44 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name = 'Maria'
+print (name)
+age = 25
+print (age)
+new_age = 4
+print (age + new_age)
+is_student = age + new_age > age
+print (is_student)
+
+is_student = True
+print (is_student)
